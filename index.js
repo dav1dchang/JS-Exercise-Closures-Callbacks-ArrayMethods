d3625e193f0e0e172b78e84ccbc5e2696186ff71
bj chang <bjchang@bjs-MacBook-Air.local>
2020-09-15 11:58:16 -0700

    Forgot to push completed project yesterday

--- index.js
+++ index.js
@@ -32,10 +32,11 @@ function processFirstItem(stringList, callback) {
  * 
  * 2. Which of the two uses a closure? How can you tell?
  * 
- * 
+ * Counter2 code uses closure because the count++ is within the scope of the function counter2, however, it references the variable 'let count = 0', which is in the outer scope.
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
+ * Counter 1 would be preferable to use in a higher-order function and counter 2 would be preferable to use as a callback function.
 */
 
 // counter1 code
@@ -62,11 +63,11 @@ Write a function called `inning` that generates a random number of points that a
 
 function inning(score){
 
-  return score = `The team scored ${Math.floor(Math.random() * 3)} runs in an inning`;
+  return score = Math.floor(Math.random() * 3);
 
 }//function inning
 
-console.log(inning())//invoking function and logging the result
+console.log(`The team scored ${inning()} runs in an inning`)//invoking function and logging the result
 
 /* Task 3: finalScore()
 
@@ -82,23 +83,34 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(callback, inningNumber){
+//changed name of callback function
+function runsCalc(score){
 
-  let homeTeam = callback * inningNumber
-  let awayTeam = callback * inningNumber
+  return score = Math.floor(Math.random() * 3);
 
-  return `Home: ${homeTeam}`,
-         `Away: ${awayTeam}`;
+}
+
+let homeTeam = 0;
+let awayTeam = 0;
+
+function finalScore(inningScore, inning){
+
+  for (let i = 0; i < inning; i++) {
+
+    homeTeam += runsCalc();
+    awayTeam += runsCalc();
+
+  }//for
+
+  let score = {
+               'Home': homeTeam,
+               'Away': awayTeam,
+  } 
+  return score;
 
 }//function finalScore
 
-function inning(score){
-
-  return score = Math.floor(Math.random() * 3)//removed text part of above function
-
-}//function inning
-
-console.log(score, 9)
+console.log(finalScore(runsCalc, 9))
 
 /* Task 4: 
 
@@ -122,8 +134,36 @@ and returns the score at each pont in the game, like so:
 
 Final Score: awayTeam - homeTeam */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+//random runs calculator per inning taken from task 2
+function runsCalc(score){
+
+  return score = Math.floor(Math.random() * 3);
+
 }
 
+let home = 0;
+let away = 0;
 
+function scoreboard(inningScore, inning) {
+
+  let currentInning = 0;
+  for (let i = 0; i < inning; i++){
+
+    currentInning++;//adding incremental innings
+    console.log(`Inning #${currentInning}: ${inningScore()}`)
+
+  }//for
+
+  return console.log(`Final Score: Away Team: ${away} - Home Team: ${home}`);
+
+}//function scoreboard
+
+let inningScore = function(){
+  
+    home += runsCalc();
+    away += runsCalc();
+    return `${away} - ${home}`
+
+}//inningScore
+
+scoreboard(inningScore, 9);//invoking function
